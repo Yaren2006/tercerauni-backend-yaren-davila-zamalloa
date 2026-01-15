5b2a78b83189eb4f3c00fe8e81125c0dbe46e695
Yaren Davila <222166@unamba.edu.pe>
2026-01-15 00:12:59 -0500

    implementacion de los repositorios

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+//Product//
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Product;
 import java.util.List;
--- src/main/java/com/example/demo/repository/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-public class ProductService {
-
-	public ProductService() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
